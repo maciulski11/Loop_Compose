397bbf10845d35d582fe18eb32ce199c7727770d
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2024-01-27 14:49:56 +0100

    implement image from firebase

--- app/src/main/java/com/example/loop_new/domain/model/firebase/Story.kt
+++ app/src/main/java/com/example/loop_new/domain/model/firebase/Story.kt
@@ -3,7 +3,8 @@ package com.example.loop_new.domain.model.firebase
 data class Story(
     val title: String? = null,
     val text: List<TextContent>? = null,
-    val uid: String? = null
+    val uid: String? = null,
+    val image: String? = null
 )
 
 data class TextContent(
--- app/src/main/java/com/example/loop_new/presentation/screens/story/StoryScreen.kt
+++ app/src/main/java/com/example/loop_new/presentation/screens/story/StoryScreen.kt
@@ -1,32 +1,40 @@
 package com.example.loop_new.presentation.screens.story
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
 import com.example.loop_new.domain.model.firebase.Story
 import com.example.loop_new.presentation.navigation.NavigationSupport
+import com.example.loop_new.ui.theme.White
+
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
@@ -57,16 +65,36 @@ fun StoryScreen(navController: NavController, viewModel: StoryViewModel) {
 @Composable
 fun StoryItem(story: Story, onClick: () -> Unit) {
 
-    Box(
+    val image = rememberImagePainter(data = story.image)
+
+    Column(
         modifier = Modifier
-            .height(174.dp)
+            .height(200.dp)
             .fillMaxWidth()
-            .padding(4.dp)
-            .background(Color.Green, shape = RoundedCornerShape(4.dp))
+            .padding(6.dp)
+            .background(White, shape = RoundedCornerShape(4.dp))
             .clickable {
                 onClick()
             }
     ) {
-        Text(text = story.title.toString())
+
+
+
+        Image(
+            painter = image,
+            contentDescription = "Image",
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .padding(bottom = 4.dp)
+                .clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop // Dostosuj skalowanie obrazka
+        )
+
+        Text(
+            text = story.title.toString(),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 2.dp)
+        )
     }
 }
