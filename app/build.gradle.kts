eacbb5d165f8ad11ac9c5cfa44806fd56bb5bb4b
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2024-02-20 18:41:22 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,4 +98,8 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    implementation("com.github.bumptech.glide:compose:1.0.0-alpha.1")
+
 }
