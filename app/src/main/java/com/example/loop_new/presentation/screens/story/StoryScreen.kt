9f1ce655ca87e004d863924ce32ecaf64d5af79f
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2024-02-22 18:55:46 +0100

    implement glide to small picture

--- app/src/main/java/com/example/loop_new/presentation/screens/story/StoryScreen.kt
+++ app/src/main/java/com/example/loop_new/presentation/screens/story/StoryScreen.kt
@@ -1,40 +1,39 @@
 package com.example.loop_new.presentation.screens.story
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import coil.compose.ImagePainter
-import coil.compose.rememberImagePainter
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.loop_new.domain.model.firebase.Story
 import com.example.loop_new.presentation.navigation.NavigationSupport
+import com.example.loop_new.ui.theme.Gray
 import com.example.loop_new.ui.theme.White
-
 
 @Preview(showSystemUi = true, showBackground = true)
 @Composable
@@ -48,48 +47,80 @@ fun StoryScreen(navController: NavController, viewModel: StoryViewModel) {
 
     BackHandler { /* gesture return is off */ }
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(3),
+    val categoryList by viewModel.categoryList.collectAsState()
+
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(2.dp),
+            .padding(16.dp)
     ) {
-        items(viewModel.storyList) { story ->
-            StoryItem(story = story) {
-                navController.navigate("${NavigationSupport.ReadScreen}/${story.uid}")
+        categoryList.forEach { category ->
+            item {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        text = category.name,
+                        style = MaterialTheme.typography.h5,
+                        modifier = Modifier
+                            .padding(bottom = 8.dp)
+                            .fillMaxWidth()
+                            .weight(1f)
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .border(0.25.dp, Gray, RoundedCornerShape(6.dp))
+                            .padding(4.dp),
+                        text = "Check All >",
+                        fontSize = 12.sp,
+                        color = Gray
+                    )
+                }
+                LazyRow(
+                    modifier = Modifier
+                        .height(200.dp)
+                        .fillMaxWidth(),
+                    contentPadding = PaddingValues(horizontal = 8.dp)
+                ) {
+                    category.stories.forEach { story ->
+                        item {
+                            StoryItem(story = story) {
+                                navController.navigate("${NavigationSupport.ReadScreen}/${story.uid}")
+                            }
+                        }
+                    }
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun StoryItem(story: Story, onClick: () -> Unit) {
 
-    val image = rememberImagePainter(data = story.image)
-
     Column(
         modifier = Modifier
-            .height(200.dp)
-            .fillMaxWidth()
+            .height(194.dp)
+            .width(120.dp)
             .padding(6.dp)
             .background(White, shape = RoundedCornerShape(4.dp))
             .clickable {
                 onClick()
             }
+
     ) {
 
-
-
-        Image(
-            painter = image,
-            contentDescription = "Image",
-            modifier = Modifier
-                .fillMaxSize()
-                .weight(1f)
-                .padding(bottom = 4.dp)
-                .clip(shape = RoundedCornerShape(4.dp)),
-            contentScale = ContentScale.Crop // Dostosuj skalowanie obrazka
-        )
+       GlideImage(
+           model = story.image ?: "",
+           contentDescription = "loadImage",
+           modifier = Modifier
+               .fillMaxSize()
+               .weight(1f)
+               .padding(bottom = 4.dp)
+               .clip(shape = RoundedCornerShape(4.dp)),
+           contentScale = ContentScale.Crop
+       )
 
         Text(
             text = story.title.toString(),
--- app/src/main/java/com/example/loop_new/presentation/screens/story/StoryViewModel.kt
+++ app/src/main/java/com/example/loop_new/presentation/screens/story/StoryViewModel.kt
@@ -1,24 +1,26 @@
 package com.example.loop_new.presentation.screens.story
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.loop_new.domain.model.firebase.Story
+import com.example.loop_new.domain.model.firebase.Category
 import com.example.loop_new.domain.services.FirebaseService
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class StoryViewModel(private val firebaseService: FirebaseService) : ViewModel() {
 
-    val storyList = mutableStateListOf<Story>()
+    private val _categoryList = MutableStateFlow<List<Category>>(emptyList())
+    val categoryList: StateFlow<List<Category>> = _categoryList
 
     init {
-        fetchListOfStory()
+        fetchListOfStoriesForAllCategories()
     }
 
-    private fun fetchListOfStory() {
+    private fun fetchListOfStoriesForAllCategories() {
         viewModelScope.launch {
-            firebaseService.fetchListOfStory().collect { loadedStory ->
-                storyList.addAll(loadedStory)
+            firebaseService.fetchListOfStory().collect { loadedCategory ->
+                _categoryList.value = _categoryList.value + loadedCategory
             }
         }
     }
