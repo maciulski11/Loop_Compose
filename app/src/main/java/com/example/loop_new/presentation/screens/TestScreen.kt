1ee04de0f4a545c80016a3b8c1bf911f2ce87e9c
maciulski11 <87575467+maciulski11@users.noreply.github.com>
2024-03-11 21:43:27 +0100

    test new functions

--- app/src/main/java/com/example/loop_new/presentation/screens/TestScreen.kt
+++ app/src/main/java/com/example/loop_new/presentation/screens/TestScreen.kt
@@ -1,120 +1,95 @@
 package com.example.loop_new.presentation.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
-import com.example.loop_new.domain.model.firebase.Story
+import com.example.loop_new.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-
-data class Category(val name: String, val books: List<String>)
-
+@Preview
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun TestScreen(categories: List<Category>) {
-// W zakresie testowym możesz zdefiniować stałe dane
-    val categories = listOf(
-        Category(
-            name = "Action",
-            books = listOf("Book 1", "Book 2", "Book 3")
-        ),
-        Category(
-            name = "Drama",
-            books = listOf("Book A", "Book B", "Book C")
-        ),
-        Category(
-            name = "Science Fiction",
-            books = listOf("Book X", "Book Y", "Book Z")
-        ),
-        // Dodaj więcej kategorii według potrzeb
-    )
+private fun CustomScreen() {
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberBottomDrawerState(initialValue = BottomDrawerValue.Closed)
 
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(paddingValues = PaddingValues(0.dp)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
-        items(categories) { category ->
-            this@LazyColumn.item {
-                Text(
-                    text = category.name,
-                    style = MaterialTheme.typography.h5,
-                    modifier = Modifier.padding(bottom = 8.dp)
-                )
-            }
-            this@LazyColumn.item {
-                LazyRow(
-                    modifier = Modifier
-                        .height(120.dp)
-                        .fillMaxWidth(),
-                    contentPadding = PaddingValues(horizontal = 8.dp)
+
+        Button(onClick = { scope.launch { drawerState.open() } }) {
+        }
+        BottomDrawerContent(drawerState, scope)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun DrawerOpenerButton(drawerState: BottomDrawerState, scope: CoroutineScope) {
+    Button(onClick = { scope.launch { drawerState.open() } }) {
+        Text("open")
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun BottomDrawerContent(drawerState: BottomDrawerState, scope: CoroutineScope) {
+    BottomDrawer(
+        drawerContent = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                Button(
+                    onClick = { scope.launch { drawerState.close() } },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Red
+                    )
                 ) {
-                    items(category.books) { book ->
-                        BookItem(book = book)
-                    }
+                    Text("up")
+                }
+                Button(
+                    onClick = { scope.launch { drawerState.expand() } },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Green
+                    )
+                ) {
+                    Text("down")
                 }
             }
-        }
-    }
+        },
+        drawerState = drawerState,
+    ) {}
 }
 
-@Composable
-fun BookItem(book: String) {
-    Card(
-        modifier = Modifier
-            .padding(8.dp)
-            .width(120.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Gray)
-        ) {
-            Text(
-                text = book,
-                style = MaterialTheme.typography.body1,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .background(Color.White)
-            )
-        }
-    }
-}
 
-@Preview(showBackground = true)
-@Composable
-fun LazyColumnWithLazyRowsPreview() {
-    val categories = List(6) { index ->
-        Category(
-            name = "Category $index",
-            books = List(10) { "Book $index-$it" }
-        )
-    }
 
-    TestScreen(categories)
-}
+
+
+
+
+
+
+
 
 
 
